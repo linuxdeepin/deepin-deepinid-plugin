b285b1d97ef87432586196758707c7afa9e91981
tsic404 <liuheng@deepin.org>
2023-08-17 15:13:30 +0800

    chore(CI): fix reuse
    log: as title

--- dcc-deepinid-plugin/pages/wechatobject.h
+++ dcc-deepinid-plugin/pages/wechatobject.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2018-2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 #ifndef WECHATOBJECT_H
 #define WECHATOBJECT_H
 
--- dcc-deepinid-plugin/syncplugin.h
+++ dcc-deepinid-plugin/syncplugin.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2018-2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 #ifndef SYNCPLUGIN_H
 #define SYNCPLUGIN_H
 #include "interface/plugininterface.h"
